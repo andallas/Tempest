6261a38d348c7ddb52b890bc183d397246b46cb1
Justin Hammond <justin.35f@gmail.com>
2013-09-03 15:49:37 -0700

    Added clock class, began implementing Physics Components.

---
+++ tempest/Clock.java
@@ -0,0 +1,49 @@
+package tempest;
+
+public class Clock
+{
+    private long PREVIOUS_TIME;
+    private long ELAPSED_TIME = 0;
+    private long TICK_TIME = 0;
+    private float DELTA_TIME = 0;
+    private int FPS = 0;
+    private int TICK = 1;
+    
+    public Clock()
+    {
+        PREVIOUS_TIME = getTime();
+    }
+    
+    public void update()
+    {   
+        long curTime = getTime();
+        ELAPSED_TIME = curTime - PREVIOUS_TIME;
+        PREVIOUS_TIME = curTime;
+        
+        DELTA_TIME = (float)(ELAPSED_TIME / 1000.0);
+        
+        TICK_TIME += DELTA_TIME;
+        
+        if(TICK_TIME >= (TICK / 20))
+        {
+            TICK++;
+            if(TICK >= 20)
+            {
+                TICK_TIME = 0;
+                TICK = 0;
+            }
+        }
+        
+        if(TICK % 5 == 0)
+        {
+            FPS = (int)Math.floor(1.0f / DELTA_TIME);
+        }
+    }
+    
+    private long getTime() { return System.nanoTime() / 1000000; }
+    
+    // Getters
+    public float DeltaTime() { return DELTA_TIME; }
+    public int Tick() { return TICK; }
+    public int FPS() { return FPS; }
+}
